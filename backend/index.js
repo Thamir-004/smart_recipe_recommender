17377536e3ea2e3d52ddf7cb6579818c0992c6e6
Thamir! <151440036+PremiumHotel@users.noreply.github.com>
2025-09-05 16:19:37 +0300

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,30 @@ import cors from "cors";
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
 app.get("/", (req,res) => {
 res.send("Backend is running!");
 });
 
 app.post("/api/get-recipe", (req, res)  => {
-    const { ingridieents } = req.body;
-    condsole.log ("Recieved the users Ingridients:", ingridients);
+    const { ingridients } = req.body;
+    console.log ("Recieved the users Ingridients:", ingridients);
 
-    res.json({
-        title: "Test Recipe",
-        ingridients: ingridients.split(","),
-        steps: ["do this , do that"],
+    res.json({  
+        "recipes":[
+       { 
+        "title": "Test Recipe",
+        "ingridients": [ingridients.split(",")],
+        "steps": ["do this , do that"], },
+        { 
+            "title": "Test Recipe",
+            "ingridients": [ingridients.split(",")],
+            "steps": ["do this , do that"], 
+        }
+        ]
     });
 });
 
 app.listen(PORT, () =>  {
-    console.log(`server running`);
+    console.log(`server running  at PORT:5000`);
 });
